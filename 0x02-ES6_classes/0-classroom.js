7287c3d668933a930d9a4ab2de4055df073d1c20
BelloAbdulmujib <belloabdulmujib3@gmal.com>
2024-06-27 17:32:51 -1100

    0x02-ES6_classes

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,6 +1,10 @@
-/* a class that accepts one attribute */
+/**
+ *  a class that accepts one attribute
+ */
 export default class ClassRoom {
-    // 
+    /**
+     * this creates a new see Link
+     */
     constructor(maxStudentsSize) {
         this._maxStudentsSize = maxStudentsSize;
     }
