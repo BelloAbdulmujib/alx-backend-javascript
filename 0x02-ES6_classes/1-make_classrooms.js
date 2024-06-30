eb619a0ecef80e6094eef1d0ef7c0d0d86e4a521
BelloAbdulmujib <belloabdulmujib3@gmal.com>
2024-06-30 00:35:36 -1100

    0x02-ES6_classes

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,4 +1,4 @@
-import ClassRoom from '0-ClassRoom.js';
+import ClassRoom from './0-clasroom';
 
 export default function initializeRooms() {
     return [19, 20,34].map((size) => new ClassRoom(size));
